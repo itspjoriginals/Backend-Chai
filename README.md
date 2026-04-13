227714e39a5d66e0397440a411835d22ae794fe0
Prashant Kumar Jha <itspjprashant07@gmail.com>
2026-04-14 01:51:13 +0530

    add folder setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project Instructions
+- Prettier installation
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Server Started at PORT : ${port}`);
+}) 
