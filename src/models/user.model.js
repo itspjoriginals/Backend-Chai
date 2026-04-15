5461b2a6e2b69e40a49e45a0050c0e01f59b30af
Prashant Kumar Jha <itspjprashant07@gmail.com>
2026-04-15 18:05:53 +0530

    Add controllers and some bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,7 +48,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function(next) {
   if(!this.isModified("password")) return next();
-  this.password = bcrpyt.hash(this.password, 10)
+  this.password = await bcrpyt.hash(this.password, 10)
   next();
 })
 
