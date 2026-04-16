e55e1e21f8c2223e10c124a1a5e185d6e77b8d2f
Prashant Kumar Jha <itspjprashant07@gmail.com>
2026-04-16 12:45:02 +0530

    feat: implement user authentication, profile management, and channel profile controllers and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverIamge } from "../controllers/user.controller.js";
 const router = Router();
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,5 +22,14 @@ router.route("/register").post(
   //secured routes
   router.route("/logout").post(verifyJWT, logoutUser);
   router.route("/refresh-token").post(refreshAccessToken)
+  router.route("/change-passoword").post(verifyJWT, changeCurrentPassword);
+  router.route("/current-user").get(verifyJWT, getCurrentUser);
+  router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+  router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+  router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverIamge);
+  router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+  router.route("/history").get(verifyJWT, getWatchHistory);
+
+  
 
 export default router;
